bfb37f750acc4b449dd10e63fc7f0b033694b5bc
Naaav64 <92137555+Naaav64@users.noreply.github.com>
2024-03-19 12:50:10 -0400

    v1.0

--- new_localuser.ps1
+++ new_localuser.ps1
@@ -5,12 +5,12 @@ $fullname = Read-Host -prompt "Enter Full Name"
 $pass = Read-Host -prompt "Enter User Password"  -AsSecureString   
 $desc = Read-Host -prompt "Enter User Title and Department"
 
-#if you want to modify user's local gropup
-# $role = #'user' or 'admininistrator' 
+#if you want to modify user's local group, uncomment
+#$role = 'Administrators' 
 
-import-module microsoft.powershell.localaccounts -UseWindowsPowerShell
+Import-Module microsoft.powershell.localaccounts -UseWindowsPowerShell 
 
-$user = New-LocalUser -name $name -Password $pass -FullName $fullname -Description $desc
+New-LocalUser -name $name -Password $pass -FullName $fullname -Description $desc
 
-#If $role is uncommented, then following line is useable.
-# Add-LocalGroupMember -group $role -Member $user
+#If $role is uncommented, then following should be as well.
+#Add-LocalGroupMember -group $role -Member $name
