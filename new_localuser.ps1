05996eba5793d609fde104a5eae6668f38c2c957
Naaav64 <92137555+Naaav64@users.noreply.github.com>
2024-03-19 12:16:03 -0400

    Made Local group assignment optional

--- new_localuser.ps1
+++ new_localuser.ps1
@@ -1,13 +1,16 @@
-#Enter Actual User Info Below
+#Enter Actual User Info As Prompted
 
 $name = Read-Host -prompt "Enter Account Name"
 $fullname = Read-Host -prompt "Enter Full Name"
 $pass = Read-Host -prompt "Enter User Password"  -AsSecureString   
 $desc = Read-Host -prompt "Enter User Title and Department"
-$role = #'user' or 'admininistrator'
+
+#if you want to modify user's local gropup
+# $role = #'user' or 'admininistrator' 
 
 import-module microsoft.powershell.localaccounts -UseWindowsPowerShell
 
 $user = New-LocalUser -name $name -Password $pass -FullName $fullname -Description $desc
 
-Add-LocalGroupMember -group $role -Member $user
+#If $role is uncommented, then following line is useable.
+# Add-LocalGroupMember -group $role -Member $user
