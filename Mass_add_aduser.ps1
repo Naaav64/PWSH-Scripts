2dd90b22ef172c3c3995d6efe2f7d08a4fc1c2e1
Naaav64 <92137555+Naaav64@users.noreply.github.com>
2024-03-26 16:26:11 -0400

    Add Users to AD from .CSV file

---
+++ Mass_add_aduser.ps1
@@ -0,0 +1,25 @@
+#Add or modify columns/parameters as needed for AD users
+
+# Import AD Module
+Import-Module ActiveDirectory
+
+#Get file path to csv
+$filepath = read-host -Prompt "Enter File Path to user inventory"
+
+#Add temporary password for all accounts
+$creds = Read-Host -Prompt "Enter Temporary Password for Bulk Account Creation" -AsSecureString
+
+#  Added trim in case "Copy as Path" used, otherwise breaks.
+$users = Import-Csv $filepath.trim('"')
+
+Foreach ($user in $users) {
+    New-ADUser `
+        -name ($user.'First Name' + " " + $user.'Last Name') `
+        -GivenName ($user.'First Name') `
+        -Surname ($user.'Last Name') `
+        -UserPrincipalName ($user.'First Name' + '.' + $user.'Last Name') `
+        -AccountPassword $creds `
+        -Path $user.'Organizational Unit' `
+        -ChangePasswordAtLogon $true `
+        -Enabled $true # Should leave disabled for security purposes, can enable as users onboard.      
+}
