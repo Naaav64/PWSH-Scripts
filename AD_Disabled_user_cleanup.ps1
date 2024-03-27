c86c259fecd7ada9f5fd50ee415abf42439510b0
Naaav64 <92137555+Naaav64@users.noreply.github.com>
2024-03-27 05:40:47 -0400

    Moves  disabled user accounts to designated OU

---
+++ AD_Disabled_user_cleanup.ps1
@@ -0,0 +1,8 @@
+#Import AD Module
+Import-Module ActiveDirectory
+
+# Moves all disabled user accounts to Disabled OU
+Search-ADAccount -AccountDisabled | Where {$_.distinguishedname -notlike "*OU=KenLab Disabled*"} | Move-ADObject -TargetPath "OU=KenLab Disabled,OU=KenLab,DC=ad,DC=kenlab,DC=com"
+
+# IFf any enabled user accounts are in the disabled OU, disables them.
+Get-ADUser -Filter {enabled -eq $true} -SearchBase "OU=KenLab Disabled,OU=KenLab,DC=ad,DC=kenlab,DC=com" | Disable-ADAccount
